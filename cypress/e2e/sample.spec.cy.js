9c5566cbf1614867530bf0a84f76413d586d6c7e
Komal <144211225+KomalkN@users.noreply.github.com>
2024-12-09 16:37:42 +0530

    fix: updare e2e

--- cypress/e2e/sample.spec.cy.js
+++ cypress/e2e/sample.spec.cy.js
@@ -1,7 +1,22 @@
 // cypress/integration/sample.spec.js
+// describe('Sample Cypress Test', () => {
+//   it('Visits the Cypress website', () => {
+//     cy.visit('https://www.cypress.io')
+//     cy.contains('Kitchen Sink').click()
+//   })
+// })
+
 describe('Sample Cypress Test', () => {
+  // Ignore specific uncaught exceptions
+  Cypress.on('uncaught:exception', (err) => {
+    if (err.message.includes("Cannot read properties of null")) {
+      return false;
+    }
+  });
+
   it('Visits the Cypress website', () => {
-    cy.visit('https://www.cypress.io')
-    cy.contains('Kitchen Sink').click()
-  })
-})
+    cy.visit('https://www.cypress.io');
+    cy.contains('Kitchen Sink', { timeout: 10000 }).should('be.visible').click();
+  });
+});
+
